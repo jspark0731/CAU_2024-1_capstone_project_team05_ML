2e95f6559ce3277ebcfbf1e260d932fdb92221ca
jspark0731 <parkjunseok0731@gmail.com>
2024-06-10 13:11:31 +0900

    DB Connection.

--- main.py
+++ main.py
@@ -1,27 +1,58 @@
 import os
+import shutil
 import uvicorn
-from fastapi import HTTPException
+from fastapi import FastAPI, HTTPException, Depends, Path
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
-from spleeter.separator import Separator
+from databases import SessionLocal
+from sqlalchemy.orm import Session
 import logging
 
+from spleeter.separator import Separator
+from basic_pitch.inference import predict_and_save
+
 from CorsConfig import app
+from database import engineconn
+from models import MLEntity
+
+engine = engineconn()
+session = engine.sessionmaker()
 
 class MLDto(BaseModel):
+    id: int
     model: str
     instrumentType: str
     fileName: str
     filePath: str
     email: str
+    generateSheet: str
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# # 파일 저장을 위한 임시 디렉터리 생성
+# def save_upload_file_tmp(upload_file: UploadFile, path: str) -> None:
+#     try:
+#         with open(path, "wb") as buffer:
+#             shutil.copyfileobj(upload_file.file, buffer)
+#     finally:
+#         upload_file.file.close()
 
 @app.post("/api/ml/getFile")
 async def getDto(mlDto: MLDto):
-    inputFilePath = mlDto.filePath
-    fileName = mlDto.fileName
-    email = mlDto.email
+    id = mlDto.id
     model = mlDto.model
+    instrumentType = mlDto.instrumentType
+    fileName = mlDto.fileName
+    inputFilePath = mlDto.filePath
+    email = mlDto.email
+    generateSheet = mlDto.generateSheet
+
+    music_entry = db.query(MLEntity).filter(MLEntity.id == mlDto.id).first()
 
     # spleeter의 결과 path 정의
     spleeterOutputPath = f"/spleeter_output/{email}/{fileName}"
@@ -35,17 +66,30 @@ async def getDto(mlDto: MLDto):
             separator = Separator(f'spleeter:{model}')
             separator.separate_to_file(inputFilePath, spleeterOutputPath)
 
-            return JSONResponse(status_code=200, content={"message": f"File '{fileName}' processed successfully."})
-        except HTTPException as e:
-            raise e
+            if generateSheet == "O":
+                instrumentFileName = "others.wav" if mlDto.instrumentType == "guitar" else f"{mlDto.instrumentType}.wav" # instrumentType이 guitar면 others.wav를 사용해야함.
+                basicPitchInputPath = os.path.join(spleeterOutputPath, instrumentFileName)
+
+                predict_and_save(
+                    [basicPitchInputPath],
+                    basicPitchOutputPath,
+                    save_midi=True
+                )
+
+
+            else:
+                return JSONResponse(status_code=200, content={"message": f"File '{fileName}' processed successfully, results stored at {spleeterOutputPath}."})
+
+            return JSONResponse(status_code=200, content={"message": f"MIDI file generated successfully at {basicPitchOutputPath}"})
         except Exception as e:
-            logging.exception("An unexpected error occurred")
-            return JSONResponse(status_code=500, content={"message": f"An error occurred: {str(e)}"})
+            logging.exception("An unexpected error occurred during separation")
+            return JSONResponse(status_code=500, content={"message": f"An error occurred during separation: {str(e)}"})
     except HTTPException as e:
         raise e
     except Exception as e:
-        logging.exception("An unexpected error occurred")
-        return JSONResponse(status_code=500, content={"message": f"An error occurred: {str(e)}"})
+        logging.exception("An unexpected error occurred during file handling")
+        return JSONResponse(status_code=500, content={"message": f"An error occurred during file handling: {str(e)}"})
+
 
 if __name__ == "__main__":
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
